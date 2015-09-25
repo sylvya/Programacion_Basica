c559e688a07901f121d1f5e62b5a980294b83779
sylvya <silvia_9010@hotmail.com>
2015-09-25 14:59:33 -0500

    Pokemon terminado

--- ObjNavegadorYobjJS/static/micodigo.js
+++ ObjNavegadorYobjJS/static/micodigo.js
@@ -78,10 +78,26 @@ navigator.geolocation.getCurrentPosition( mostrar );
 
 var pikachu = Pokemon("Pikachu", 100, 55 );
 var bulbasaur = Pokemon("Bulbasaur", 90, 50);*/
+var pikachu3 = {
+	nombre:"pikachu",
+	vida:100,
+	ataque:55,
+	grito:"pikaaaa!!!!",
+	imagen:"../static/pikachu.jpg"
+}
+var bulbasaur3 = {
+	nombre:"Bulbasaur",
+	vida:90,
+	ataque:50,
+	grito:"nose!!!!",
+	imagen:"../static/bulbasaur.jpg"
+}
 
-function pokemon(n,v,a)
+
+
+function pokemon(n,v,a,g)
 {
-	this.grito="Pika";
+	this.grito=g;
 	this.nombre=n;
 	this.vida=v;
 	this.ataque=a;
@@ -91,23 +107,49 @@ function pokemon(n,v,a)
 	}
 }
 function inicio(){
-	alert("Esto es depués del inicio");
-	var rattata = new pokemon("Rattata", 40, 2);
-	rattata.vida=rattata.vida-13;
-	rattata.grito = "Paredes!!";
-	nombrePokemon.innerText = rattata.nombre;
 
+	var seleccion = prompt("Elige tu Pokempon \n  1=Pikachu\n 2=Bulbasaur " );
+	validaPokemon(seleccion);
+	//var rattata = new pokemon("Rattata", 40, 2);
+	//rattata.vida=rattata.vida-13;
+	//rattata.grito = "Paredes!!";
+	//nombrePokemon.innerText = rattata.nombre;
 
-}
-function datos(){
-	var pikachu = new pokemon("Pikachu", 100, 55);
-	document.write("nombre: "+pikachu.nombre, "</br>Vida: "+pikachu.vida, "</br>Ataque: "+pikachu.ataque,  "</br>Grito: "+pikachu.grito);
+	//datos();
 
 }
+//function datos(){
+	//var pikachu1 = new pokemon("Pikachu", 100, 55, "Pika");
+	//document.write("nombre: "+pikachu1.nombre, "</br>Vida: "+pikachu1.vida, "</br>Ataque: "+pikachu1.ataque,  "</br>Grito: "+pikachu1.grito);
+	//pikachu1.gritar();
+	//   }
+
+function validaPokemon(selec){
+
+	if (selec==1)
+	 {
+	 	var pikachu1 = new pokemon(pikachu3.nombre, pikachu3.vida, pikachu3.ataque, pikachu3.grito);
+	document.write("nombre: "+pikachu1.nombre, "</br>Vida: "+pikachu1.vida, "</br>Ataque: "+pikachu1.ataque, "</br>Grito: "+pikachu1.grito);
+	nombrePokemon.innerText = pikachu1.nombre;
+	var texto="</br><img src='"+ pikachu3.imagen +"' alt='Imagen' />";
+    document.write(texto);
+    
+	 }
+	 else if (selec==2) { 
+
+	 	var bulbasaur1 = new pokemon("Bulbasaur", 90, 50,"nose");
+	document.write("nombre: "+bulbasaur1.nombre, "</br>Vida: "+bulbasaur1.vida, "</br>Ataque: "+bulbasaur1.ataque, "</br>Grito: "+bulbasaur1.grito);
+	 nombrePokemon.innerText = bulbasaur1.nombre;
+	 var texto="</br><img src='"+ bulbasaur3.imagen +"' alt='Imagen' />";
+	 document.write(texto);
+	 }
+	 else{
+	 	alert("Error");
+	 }
+}
 
 
 
-alert("esto es antes del inicio");
 
 //en id datos de prueba ataque vida y grito del pokemon
 //en base al nombre del po kemon  crear imagen adjuntar la imagen del pokemon
---
+++ pokemon/static/pokemon.js
@@ -0,0 +1,9 @@
+
+var pikachu={
+
+	nombre:"Pikachu",
+	vida:100,
+	ataque:55,
+	grito:"Pikaaaaa!!!!",
+	imagen:"../static/"
+}
